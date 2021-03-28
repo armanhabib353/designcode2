760b45d8a72ca43798cd6616272a1c97c4cae920
Arman Asha <armanasha@Armans-Mac-Pro.local>
2021-03-29 01:30:00 +0600

    class1

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,43 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+              child: Container(
+                child: Row(
+                  children: [
+                    Container(
+                      height: 42,
+                      width: 42,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: LinearGradient(colors: [
+                          Color(0xFF64CEFF),
+                          Color(0xFF64FFDA),
+                        ]),
+                      ),
+                      child: Icon(Icons.home),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      "Home",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
