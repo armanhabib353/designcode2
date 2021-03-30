fd23e863bae484948409ccd0067d7c16eeab5061
Arman Asha <armanasha@Armans-Mac-Pro.local>
2021-03-30 12:01:31 +0600

    class 5

--- lib/components/cards/recent_course_card.dart
+++ lib/components/cards/recent_course_card.dart
@@ -11,79 +11,77 @@ class RecentCourseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        alignment: Alignment.topRight,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 30),
-            child: Container(
-              height: 240,
-              width: 240,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(32),
-                  gradient: course.background,
-                  boxShadow: [
-                    BoxShadow(
-                      color: course.background.colors[0].withOpacity(0.3),
-                      offset: Offset(0, 4),
-                      blurRadius: 16,
-                    ),
-                    BoxShadow(
-                      color: course.background.colors[1].withOpacity(0.3),
-                      offset: Offset(0, 4),
-                      blurRadius: 16,
-                    ),
-                  ]
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 32, left: 32, right: 32),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(course.courseTitle, style: kCardSubtitleStyle),
-                        Text(
-                          course.courseTitle,
-                          style: kCardTitleStyle,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Image.asset(
-                      "asset/illustrations/${course.illustration}",
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 45),
-            child: Container(
-              child: Image.asset(
-                'asset/logos/${course.logo}',
-              ),
-              height: 60,
-              width: 60,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 30),
+          child: Container(
+            height: 240,
+            width: 240,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(32),
+                gradient: course.background,
                 boxShadow: [
                   BoxShadow(
-                    color: kShadowColor,
+                    color: course.background.colors[0].withOpacity(0.3),
                     offset: Offset(0, 4),
-                    blurRadius: 16.0,
+                    blurRadius: 16,
                   ),
-                ],
-              ),
+                  BoxShadow(
+                    color: course.background.colors[1].withOpacity(0.3),
+                    offset: Offset(0, 4),
+                    blurRadius: 16,
+                  ),
+                ]
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 32, left: 32, right: 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(course.courseTitle, style: kCardSubtitleStyle),
+                      Text(
+                        course.courseTitle,
+                        style: kCardTitleStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    "asset/illustrations/${course.illustration}",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 45),
+          child: Container(
+            child: Image.asset(
+              'asset/logos/${course.logo}',
+            ),
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: kShadowColor,
+                  offset: Offset(0, 4),
+                  blurRadius: 16.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learning/constants.dart';
 import 'package:learning/model/course.dart';
 import 'components/cards/recent_course_card.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -14,8 +14,29 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-          child: RecentCourseCard(
-            course: recentCourses[0],
+          color: kBackgroundColor,
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HomeScreenNavBar(),
+                Padding(
+                  padding: EdgeInsets.only(left: 25),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Recent", style: kLargeTitleStyle,),
+                      Text("This Course is 13 Section", style: kSubtitleStyle,),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: EdgeInsets.only(left: 25),
+                  child: RecentCourseCard(course: recentCourses[0]),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -23,3 +44,96 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class HomeScreenNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SidebarButton(),
+          SearchFieldWidget(),
+          Icon(Icons.notifications),
+          SizedBox(width: 14,),
+          CircleAvatar(backgroundImage: AssetImage('asset/images/profile.jpg'),)
+        ],
+      ),
+    );
+  }
+}
+
+class SearchFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 12, right: 25),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14),
+              boxShadow: [
+                BoxShadow(
+                    color: kShadowColor,
+                    offset: Offset(0, 12),
+                    blurRadius: 16)
+              ]),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: TextField(
+              cursorColor: kPrimaryLabelColor,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Search For it",
+                icon: Icon(
+                  Icons.search,
+                  color: kPrimaryLabelColor,
+                  size: 20,
+                ),
+                hintStyle: kSearchPlaceholderStyle,
+              ),
+              style: kSearchTextStyle,
+              onChanged: (newText) {
+                print(newText);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class SidebarButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        print('side bar button pressed');
+      },
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: BoxConstraints(maxWidth: 45, maxHeight: 45),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14),
+            boxShadow: [
+              BoxShadow(
+                color: kShadowColor,
+                offset: Offset(0, 12),
+                blurRadius: 16,
+              ),
+            ]),
+        child: Image.asset(
+          'asset/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+        padding: EdgeInsets.all(10),
+      ),
+    );
+  }
+}
