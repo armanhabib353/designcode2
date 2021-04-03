27744d5ecd7ddb16c0c4fce8dd7302361c1cdf1f
Arman Asha <armanasha@Armans-Mac-Pro.local>
2021-04-03 11:48:13 +0600

    class 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -140,8 +142,138 @@ class _CourseScreenState extends State<CourseScreen> {
                       ),
                     ),
                   ),
+                  
+                  Padding(padding: EdgeInsets.only(right: 28.0),
+                  child: Container(
+                    padding: EdgeInsets.only(
+                      top: 12.5,
+                      bottom: 13.5,
+                      left: 20.5,
+                      right: 14.5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: kShadowColor,
+                          offset: Offset(0, 4),
+                          blurRadius: 16,
+                        ),
+                      ],
+                    ),
+                    height: 60.0,
+                    width: 60.0,
+                    child: Image.asset('asset/icons/icon-play.png'),
+                  ),
+                  ),
                 ],
-              )
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 12.0,
+                  left: 28.0,
+                  right: 28.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                     Row(
+                       children: [
+                         Container(
+                           child: Padding(
+                             padding: EdgeInsets.all(4.0),
+                             child: Container(
+                               padding: EdgeInsets.all(4.0),
+                               child: CircleAvatar(
+                                 child: Icon(
+                                   Platform.isAndroid ? Icons.people : CupertinoIcons.group_solid,
+                                   color: Colors.white,
+                                 ),
+                                 radius: 21.0,
+                                 backgroundColor: kCourseElementIconColor,
+                               ),
+                               decoration: BoxDecoration(
+                                 color: kBackgroundColor,
+                                 borderRadius: BorderRadius.circular(29.0),
+                               ),
+                             ),
+                           ),
+                           height: 58,
+                           width: 58,
+                           decoration: BoxDecoration(
+                             gradient: widget.course.background,
+                             borderRadius: BorderRadius.circular(29.0),
+                           ),
+                         ),
+                         SizedBox(width: 12.0,),
+                         Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                             Text("28.7k", style: kTitle1Style,),
+                             Text("Student", style: kSearchPlaceholderStyle,),
+                           ],
+                         ),
+                       ],
+                     ),
+                    Row(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Container(
+                              padding: EdgeInsets.all(4.0),
+                              child: CircleAvatar(
+                                child: Icon(
+                                  Platform.isAndroid ? Icons.comment : CupertinoIcons.news_solid,
+                                  color: Colors.white,
+                                ),
+                                radius: 21.0,
+                                backgroundColor: kCourseElementIconColor,
+                              ),
+                              decoration: BoxDecoration(
+                                color: kBackgroundColor,
+                                borderRadius: BorderRadius.circular(29.0),
+                              ),
+                            ),
+                          ),
+                          height: 58,
+                          width: 58,
+                          decoration: BoxDecoration(
+                            gradient: widget.course.background,
+                            borderRadius: BorderRadius.circular(29.0),
+                          ),
+                        ),
+                        SizedBox(width: 12.0,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("12.4k", style: kTitle1Style,),
+                            Text("Comments", style: kSearchPlaceholderStyle,),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 24.0,),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 28.0,),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("As a developer, your goal should be to create designs that are as objectively good as possible, using design systems, well-established principles and techniques that translate well to code. You should work on your speed, since you don’t want to steal away from your coding time. My goal is to get you to be satisfied with your designs and to find a system to consistently improve over time by not only explaining the foundations of UI design, but my personal insights on how it's feasible to divide your time between design and code. We'll be using free icons and illustrations from", style: kBodyLabelStyle,),
+                    SizedBox(height: 24.0,),
+                    Text("About this course", style: kTitle1Style,),
+                    SizedBox(height: 24.0,),
+                    Text("As a developer, your goal should be to create designs that are as objectively good as possible, using design systems, well-established principles and techniques that translate well to code. You should work on your speed, since you don’t want to steal away from your coding time. My goal is to get you to be satisfied with your designs and to find a system to consistently improve over time by not only explaining the foundations of UI design, but my personal insights on how it's feasible to divide your time between design and code. We'll be using free icons and illustrations from", style: kBodyLabelStyle,),
+                    SizedBox(height: 30.0,),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
