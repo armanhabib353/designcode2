2c7cb53549bda67f99e6ae228ff3095828f993e9
Arman Asha <armanasha@Armans-Mac-Pro.local>
2021-04-05 00:03:05 +0600

    class 14

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learning/components/list/certificate_viewer.dart';
 import 'package:learning/constants.dart';
+import 'package:learning/model/course.dart';
 
 class ProfileScreen extends StatelessWidget {
 
@@ -184,9 +186,261 @@ class ProfileScreen extends StatelessWidget {
                 ),
               ),
             ),
+
+            Padding(
+              padding: EdgeInsets.only(
+              top: 32,
+              left: 20,
+              right: 20,
+              bottom: 12,
+            ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Certificates", style: kHeadlineLabelStyle,),
+                      Row(
+                        children: [
+                          Text("See All", style: kSearchPlaceholderStyle,),
+                          Icon(Icons.chevron_right, color: kSecondaryLabelColor,)
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            CertificateViewer(),
+
+            Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
+                bottom: 12,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Completed Courses", style: kHeadlineLabelStyle,),
+                      Row(
+                        children: [
+                          Text("See All", style: kSearchPlaceholderStyle,),
+                          Icon(Icons.chevron_right, color: kSecondaryLabelColor,)
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            CompleteCourseList(),
+            SizedBox(height: 40,),
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class CompleteCourseList extends StatefulWidget {
+  @override
+  _CompleteCourseListState createState() => _CompleteCourseListState();
+}
+
+class _CompleteCourseListState extends State<CompleteCourseList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+
+  Widget updateIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: completedCourses.map((course) {
+        var index = completedCourses.indexOf(course);
+        return Container(
+          width: 7.0,
+          height: 7.0,
+          margin: EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: currentPage == index ? Color(0xFF0971FE) : Color(0xFFA6AEBD),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 200,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return Opacity(
+                opacity: currentPage == index ? 1.0 : 0.5,
+                child: CompleteCourseCard(
+                  course: completedCourses[index],
+                ),
+              );
+            },
+            itemCount: completedCourses.length,
+            controller: PageController(initialPage: 0, viewportFraction: 0.70),
+            onPageChanged: (index) {
+              setState(() {
+                currentPage = index;
+              });
+            },
+          ),
+        ),
+        updateIndicators(),
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class CompleteCourseCard extends StatelessWidget {
+  CompleteCourseCard({@required this.course});
+
+  final Course course;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 20.0, right: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(32),
+                gradient: course.background,
+                boxShadow: [
+                  BoxShadow(
+                    color: course.background.colors[0].withOpacity(0.3),
+                    offset: Offset(0, 4),
+                    blurRadius: 20,
+                  ),
+                  BoxShadow(
+                    color: course.background.colors[1].withOpacity(0.3),
+                    offset: Offset(0, 4),
+                    blurRadius: 20,
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(41.0),
+                child: Container(
+                  height: 140.0,
+                  width: 260.0,
+                  child: Stack(
+                    children: [
+                      Row(
+                        children: [
+                          Spacer(),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Image.asset(
+                                "asset/illustrations/${course.illustration}",
+                                fit: BoxFit.cover,
+                                height: 140.0,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(32.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(course.courseTitle, style: kCardSubtitleStyle),
+                            Text(
+                              course.courseTitle,
+                              style: kCardTitleStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 45),
+            child: Container(
+              child: Image.asset(
+                'asset/icons/icon-play.png',
+              ),
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: kShadowColor,
+                    offset: Offset(0, 4),
+                    blurRadius: 16.0,
+                  ),
+                ],
+              ),
+              padding: EdgeInsets.only(
+                  top: 12.5, left: 20.5, right: 14.5, bottom: 13.5),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
