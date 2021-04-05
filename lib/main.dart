e9b5be78b19203e81c55f74b9c86bf4cd62808ef
Arman Asha <armanasha@Armans-Mac-Pro.local>
2021-04-05 11:41:11 +0600

    Complete Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:learning/constants.dart';
-import 'package:learning/model/course.dart';
 import 'package:learning/screens/home_screen.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 void main() {
   runApp(MyApp());
